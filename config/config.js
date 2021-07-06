15db7a03496c8fc2820c96d2ac89c44c04b9b71f
Leifer Mendez <leifer33@gmail.com>
2021-07-06 16:30:10 +0200

    Init

---
+++ config/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    url: 'https://m.facebook.com',
+    puppeterConfig: {
+        headless: false,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sendbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu',
+        ],
+    },
+    username: 'tecniserviciosleifer@gmail.com',
+    password: 'Token.0102',
+    useCookies: true,
+    cookiesFilePath: './cookies.json',
+    layout: {
+        login_form: {
+            email: 'input#m_login_email',
+            password: 'input[type="password"]',
+            submit: 'button[data-sigil="touchable login_button_block m_login_button"]',
+            parent: 'form',
+        },
+        facebook_group: {
+            m_group_stories_container: '#m_group_stories_container',
+            m_group_post_div: '#m_group_stories_container .storyStream article',
+            m_group_story_container: '#m_group_stories_container .storyStream article .story_body_container',
+        }
+    }
+}
