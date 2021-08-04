3606c227dd8dca1005b392052a38bb819742db6e
Leifer Mendez <leifer33@gmail.com>
2021-08-04 14:49:02 +0200

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: "fb-scrapper-members",
+            script: "./src/app.js",
+            watch: false,
+            max_memory_restart: '1000M',
+            exec_mode: "cluster",
+            instances: 1,
+            cron_restart: "0 */9 * * *"
+        }
+    ]
+}
